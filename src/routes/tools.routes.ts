58815081a9668979d7a4e7d1a4894670a83d84fd
Wellici Araujo <wja1@ifal.edu.br>
2020-05-18 00:38:37 -0300

    service for create tool

--- src/routes/tools.routes.ts
+++ src/routes/tools.routes.ts
@@ -1,9 +1,25 @@
 import { Router } from 'express';
 
+import CreateToolService from '../services/CreateToolService';
+
 const toolsRouter = Router();
 
 toolsRouter.get('/', (request, response) => {
   return response.json({ tools: [{ title: 'oi' }] });
+});
+
+toolsRouter.post('/', async (request, response) => {
+  const { title, description, link } = request.body;
+
+  const createTool = new CreateToolService();
+  const tool = await createTool.execute({
+    title,
+    link,
+    description,
+    user_id: '6a1340e5-b496-4d35-b681-a8e88f958fe2',
+  });
+
+  return response.json(tool);
 });
 
 export default toolsRouter;
---
+++ src/services/CreateToolService.ts
@@ -0,0 +1,33 @@
+import { getRepository } from 'typeorm';
+import Tool from '../models/tool';
+
+interface Request {
+  title: string;
+  link: string;
+  description: string;
+  user_id: string;
+}
+
+class CreateToolService {
+  public async execute({
+    title,
+    link,
+    description,
+    user_id,
+  }: Request): Promise<Tool> {
+    const toolRepository = getRepository(Tool);
+
+    const tool = toolRepository.create({
+      title,
+      link,
+      description,
+      user_id,
+    });
+
+    await toolRepository.save(tool);
+
+    return tool;
+  }
+}
+
+export default CreateToolService;
